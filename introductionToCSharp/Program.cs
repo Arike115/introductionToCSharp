683d280b1c363b295eebea16dbd9d4192e024d78
funke <arike406@gmail.com>
2022-06-10 19:12:37 +0100

    changes

--- introductionToCSharp/Program.cs
+++ introductionToCSharp/Program.cs
@@ -11,45 +11,78 @@ namespace introductionToCSharp
             //Statements
             //conditional-statmenet
             //switch case
-            int totalamount = 100;
-            start:
-            Console.WriteLine("Please select your menu. 1... rice, 2....beans, 3 ...chicken");
-            int result = int.Parse(Console.ReadLine());
+            //int totalamount = 100;
+            //start:
+            //Console.WriteLine("Please select your menu. 1... rice, 2....beans, 3 ...chicken");
+            //int result = int.Parse(Console.ReadLine());
 
-            switch (result)
+            //switch (result)
+            //{
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //        break;
+            //    default:
+            //        Console.WriteLine("your choice {0} is invalid", result);
+            //        break;
+
+            //}
+
+            //decide:
+            //Console.WriteLine(" Do you want to buy another item .... YES OR NO");
+            //string userdecision = Console.ReadLine();
+
+            //switch (userdecision.ToUpper())
+            //{
+            //    case "YES":
+            //        goto start;
+            //    case "NO":
+            //        break;
+            //    default:
+            //        Console.WriteLine("your choice {0} is invalid. please try again...",userdecision);
+            //        goto decide;
+
+
+            //}
+
+            //Console.WriteLine("thank you for your patronage");
+            //Console.WriteLine("Bill Amount - {0}", totalamount + result);
+
+            //loop statements
+            //while loop 
+            
+            //Console.WriteLine("please enter you lucky number");
+            //int target = int.Parse(Console.ReadLine());
+
+            //int start = 0;
+
+            //while(start <= target)
+            //{
+            //    Console.Write(start + " ");
+            //    start = start + 2;
+            //}
+            //string userchoice = "";
+
+            //do
+            //{
+            //    Console.WriteLine("do you want to continue - yes or no");
+            //    userchoice= Console.ReadLine();
+            //    if (userchoice != "yes" && userchoice != "no")
+            //    {
+            //        Console.WriteLine("invalid choice, please enter yes or no");
+            //    }
+            //} while (userchoice != "yes" && userchoice != "no");
+
+
+            //for looop
+
+            int i;
+            int x = 5;
+
+            for(i = 0; i < x; i++)
             {
-                case 1:
-                case 2:
-                case 3:
-                    break;
-                default:
-                    Console.WriteLine("your choice {0} is invalid", result);
-                    break;
-
+                Console.WriteLine("value of i:{0}", i);
             }
-
-            decide:
-            Console.WriteLine(" Do you want to buy another item .... YES OR NO");
-            string userdecision = Console.ReadLine();
-
-            switch (userdecision.ToUpper())
-            {
-                case "YES":
-                    goto start;
-                case "NO":
-                    break;
-                default:
-                    Console.WriteLine("your choice {0} is invalid. please try again...",userdecision);
-                    goto decide;
-
-
-            }
-
-            Console.WriteLine("thank you for your patronage");
-            Console.WriteLine("Bill Amount - {0}", totalamount + result);
-
-
-
 
 
 
