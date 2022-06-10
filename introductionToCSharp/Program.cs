f04b0f8230c8cbe7aa8758f8083991506433c5ce
funke <arike406@gmail.com>
2022-06-10 18:16:15 +0100

    changes

--- introductionToCSharp/Program.cs
+++ introductionToCSharp/Program.cs
@@ -3,85 +3,63 @@
 namespace introductionToCSharp
 {
     class Program
-    {
-        static void Main()
+   {
+       static void Main()
         {
-            //Console.WriteLine("input your FullName");
-            //string fullname = Console.ReadLine();
-            //Console.WriteLine("Wellcome our dear customer {0}", fullname);
-
-            //built in types
-            string names = "jsajhrjwjhr";
-            int numbers = 570;
-            bool words = true;
-            decimal number = 74;
-            double num = 45.78;
-            long allnum = 5757575757575757570;
-            float allnumbers = 65385F;
-            //Console.WriteLine(int.MaxValue);
-
-            //operators in c#
-            //Arithmetic Operators
-
-            int newnumber = 12;
-            int newnum = 15;
-
-            int result = newnumber + newnum;
-            int newresult = newnum - newnumber;
-            int mult = newnum;
-            //Console.WriteLine(newresult);
-            //Console.WriteLine(mult);
-
-            //Relational Operators
-            // ==, !=, >, <, >=,<=
-            //Console.WriteLine("Please enter a number");
-            //string fullname = Console.ReadLine();
-            //int newnumbers = 17;
-
-            //if (newnumbers >= 17)
-            //{
-            //    Console.WriteLine("the number is correct");
-
-            //}
-            //else
-            //{
-            //    Console.WriteLine("incorrect pin");
-            //}
-
-            //Logical Operators
-            // && => and , || => or, !=> not
 
 
-            string fruits = "Apple";
-            string word = "pen";
+            //Statements
+            //conditional-statmenet
+            //switch case
+            int totalamount = 100;
+            start:
+            Console.WriteLine("Please select your menu. 1... rice, 2....beans, 3 ...chicken");
+            int result = int.Parse(Console.ReadLine());
 
-            if ( fruits != "pen" || word =="chalk")
+            switch (result)
             {
-                Console.WriteLine("the words are correct");
-            }
-            else
-            {
-                Console.WriteLine("the words are incorrect");
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("your choice {0} is invalid", result);
+                    break;
+
             }
 
-            ////null coalescing operator
-            ///??
+            decide:
+            Console.WriteLine(" Do you want to buy another item .... YES OR NO");
+            string userdecision = Console.ReadLine();
 
-            int? r = null;
-            Console.WriteLine("please enter your name");
-            string myname = null;
+            switch (userdecision.ToUpper())
+            {
+                case "YES":
+                    goto start;
+                case "NO":
+                    break;
+                default:
+                    Console.WriteLine("your choice {0} is invalid. please try again...",userdecision);
+                    goto decide;
 
-            string name = myname ?? "boluwatife";
-                
-            Console.WriteLine(name);
-            Console.WriteLine("You are welcome");
+
+            }
+
+            Console.WriteLine("thank you for your patronage");
+            Console.WriteLine("Bill Amount - {0}", totalamount + result);
+
+
 
 
 
 
         }
 
+
     }
 
-    
+   
+
+
+
 }
