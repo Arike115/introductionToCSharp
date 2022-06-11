b4083cf4f138c4fb9727c072a79659aa2dd273b4
funke <arike406@gmail.com>
2022-06-11 13:26:51 +0100

    chnages

--- introductionToCSharp/Practise.cs
+++ introductionToCSharp/Practise.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace introductionToCSharp
 {
-    class Practise
+    public class customer
     {
+        string _firstname;//field
+        string _lastname;
+        int _phonenumber;
+        decimal _quantity;
+
+
+        //constructor
+        public customer(string Firstname, string Lastname, decimal total)
+        {
+            _firstname = Firstname;
+            _lastname = Lastname;
+            _quantity = total;
+        }
+
+        //method
+        public void Fullname()
+        {
+            Console.WriteLine("full name = {0}", _firstname + " " + _lastname);
+        }
+        //method
+        public void Justname()
+        {
+            Console.WriteLine("justname = {0}", _lastname);
+        }
+
     }
 }
--- introductionToCSharp/Program.cs
+++ introductionToCSharp/Program.cs
@@ -6,33 +6,9 @@ namespace introductionToCSharp
    {
        static void Main()
         {
-            //loops
-            int[] numbers = new int[5];
-            numbers[0] = 250;
-            numbers[1] = 235;
-            numbers[2] = 10;
-            numbers[3] = 78;
-            numbers[4] = 76;
-
-            foreach(int num in numbers)
-            {
-                Console.WriteLine(num);
-            }
-             Console.WriteLine("***********");
-
-            for (int j = 0; j < numbers.Length; j = j+2)
-            {
-                Console.WriteLine(numbers[j]);
-            }
-            Console.WriteLine("***********");
-            int i = 0;
-
-            while(i < numbers.Length)
-            {
-                Console.WriteLine(numbers[i]);
-                i++;
-            }
-
+            customer cust = new customer("Boluwatife", "Orezi", 1000000);
+            cust.Fullname();
+            cust.Justname();
 
         }
 
