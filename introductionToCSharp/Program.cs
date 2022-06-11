fe3713e53e0c10dc95799f2dc3f3eeeabe5d9fc5
funke <arike406@gmail.com>
2022-06-11 12:42:35 +0100

    changes

--- introductionToCSharp/Program.cs
+++ introductionToCSharp/Program.cs
@@ -6,84 +6,32 @@ namespace introductionToCSharp
    {
        static void Main()
         {
+            //loops
+            int[] numbers = new int[5];
+            numbers[0] = 250;
+            numbers[1] = 235;
+            numbers[2] = 10;
+            numbers[3] = 78;
+            numbers[4] = 76;
 
-
-            //Statements
-            //conditional-statmenet
-            //switch case
-            //int totalamount = 100;
-            //start:
-            //Console.WriteLine("Please select your menu. 1... rice, 2....beans, 3 ...chicken");
-            //int result = int.Parse(Console.ReadLine());
-
-            //switch (result)
-            //{
-            //    case 1:
-            //    case 2:
-            //    case 3:
-            //        break;
-            //    default:
-            //        Console.WriteLine("your choice {0} is invalid", result);
-            //        break;
-
-            //}
-
-            //decide:
-            //Console.WriteLine(" Do you want to buy another item .... YES OR NO");
-            //string userdecision = Console.ReadLine();
-
-            //switch (userdecision.ToUpper())
-            //{
-            //    case "YES":
-            //        goto start;
-            //    case "NO":
-            //        break;
-            //    default:
-            //        Console.WriteLine("your choice {0} is invalid. please try again...",userdecision);
-            //        goto decide;
-
-
-            //}
-
-            //Console.WriteLine("thank you for your patronage");
-            //Console.WriteLine("Bill Amount - {0}", totalamount + result);
-
-            //loop statements
-            //while loop 
-            
-            //Console.WriteLine("please enter you lucky number");
-            //int target = int.Parse(Console.ReadLine());
-
-            //int start = 0;
-
-            //while(start <= target)
-            //{
-            //    Console.Write(start + " ");
-            //    start = start + 2;
-            //}
-            //string userchoice = "";
-
-            //do
-            //{
-            //    Console.WriteLine("do you want to continue - yes or no");
-            //    userchoice= Console.ReadLine();
-            //    if (userchoice != "yes" && userchoice != "no")
-            //    {
-            //        Console.WriteLine("invalid choice, please enter yes or no");
-            //    }
-            //} while (userchoice != "yes" && userchoice != "no");
-
-
-            //for looop
-
-            int i;
-            int x = 5;
-
-            for(i = 0; i < x; i++)
+            foreach(int num in numbers)
             {
-                Console.WriteLine("value of i:{0}", i);
+                Console.WriteLine(num);
             }
+             Console.WriteLine("***********");
 
+            for (int j = 0; j < numbers.Length; j = j+2)
+            {
+                Console.WriteLine(numbers[j]);
+            }
+            Console.WriteLine("***********");
+            int i = 0;
+
+            while(i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+                i++;
+            }
 
 
         }
